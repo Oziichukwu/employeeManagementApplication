0af2794d08960879bbf5f3b5b775f3dbc3c10d1d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-16 07:12:49 +0100

    employee controller

--- src/main/java/africa/semicolon/backendemployeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/africa/semicolon/backendemployeemanagementsystem/controller/EmployeeController.java
@@ -1,25 +1,27 @@
 package africa.semicolon.backendemployeemanagementsystem.controller;
 
 import africa.semicolon.backendemployeemanagementsystem.data.dtos.request.CreateEmployeeRequest;
+import africa.semicolon.backendemployeemanagementsystem.data.models.Employee;
 import africa.semicolon.backendemployeemanagementsystem.exceptions.DuplicateEmailException;
 import africa.semicolon.backendemployeemanagementsystem.exceptions.RunTimeExceptionPlaceholder;
 import africa.semicolon.backendemployeemanagementsystem.services.EmployeeService;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
-@AllArgsConstructor
 public class EmployeeController {
 
     @Autowired
-    private final EmployeeService employeeService;
+    EmployeeService employeeService;
 
-    @RequestMapping("/api/v1/create_employee")
+    @PostMapping("/api/v1/create_employee")
     public ResponseEntity<?> createEmployee(@RequestBody CreateEmployeeRequest createEmployeeRequest){
 
         try {
@@ -28,5 +30,10 @@ public class EmployeeController {
         } catch (DuplicateEmailException | RunTimeExceptionPlaceholder e){
             return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/api/v1/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
     }
 }
