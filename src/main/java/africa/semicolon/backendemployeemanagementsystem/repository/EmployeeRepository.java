65c2d9d4bf3fe0cf03dd8446e94ed814488a975d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-16 07:10:36 +0100

    employee repository updated

--- src/main/java/africa/semicolon/backendemployeemanagementsystem/repository/EmployeeRepository.java
+++ src/main/java/africa/semicolon/backendemployeemanagementsystem/repository/EmployeeRepository.java
@@ -1,15 +1,17 @@
 package africa.semicolon.backendemployeemanagementsystem.repository;
 
+import africa.semicolon.backendemployeemanagementsystem.data.dtos.response.CreateEmployeeResponse;
 import africa.semicolon.backendemployeemanagementsystem.data.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee,String> {
 
-    Optional<Employee> findByEmployeeId(String userId);
+    //Optional<Employee> findByEmployeeId(String id);
     Boolean existsByUserName(String userName);
     Boolean existsByEmail(String email);
 
