55201040d2d7a6bbbd9cbf7bede18c4e78d4cbae
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-16 07:10:03 +0100

    interface updated

--- src/main/java/africa/semicolon/backendemployeemanagementsystem/services/EmployeeService.java
+++ src/main/java/africa/semicolon/backendemployeemanagementsystem/services/EmployeeService.java
@@ -2,13 +2,17 @@ package africa.semicolon.backendemployeemanagementsystem.services;
 
 import africa.semicolon.backendemployeemanagementsystem.data.dtos.request.CreateEmployeeRequest;
 import africa.semicolon.backendemployeemanagementsystem.data.dtos.response.CreateEmployeeResponse;
+import africa.semicolon.backendemployeemanagementsystem.data.models.Employee;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 public interface EmployeeService {
 
+    String createEmployee(CreateEmployeeRequest createEmployeeRequest);
 
-    CreateEmployeeResponse createEmployee(CreateEmployeeRequest createEmployeeRequest);
+    List<Employee> getAllEmployees();
 
 
 }
