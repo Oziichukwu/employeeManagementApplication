65d95e03dae4e5aceb4ba53b91e6698947d1943b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-24 00:45:57 +0100

    employee controller delete method added

--- src/main/java/africa/semicolon/backendemployeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/africa/semicolon/backendemployeemanagementsystem/controller/EmployeeController.java
@@ -1,12 +1,13 @@
 package africa.semicolon.backendemployeemanagementsystem.controller;
 
 import africa.semicolon.backendemployeemanagementsystem.data.dtos.request.CreateEmployeeRequest;
+import africa.semicolon.backendemployeemanagementsystem.data.dtos.response.EmployeeInfo;
 import africa.semicolon.backendemployeemanagementsystem.data.models.Employee;
 import africa.semicolon.backendemployeemanagementsystem.exceptions.DuplicateEmailException;
+import africa.semicolon.backendemployeemanagementsystem.exceptions.EmployeeNotFoundException;
 import africa.semicolon.backendemployeemanagementsystem.exceptions.RunTimeExceptionPlaceholder;
+import africa.semicolon.backendemployeemanagementsystem.repository.EmployeeRepository;
 import africa.semicolon.backendemployeemanagementsystem.services.EmployeeService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,10 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @PostMapping("/api/v1/create_employee")
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @PostMapping("api/v1/create_employee")
     public ResponseEntity<?> createEmployee(@RequestBody CreateEmployeeRequest createEmployeeRequest) {
 
         try {
@@ -36,4 +40,35 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
+    @GetMapping("/api/v1/{id}")
+    public ResponseEntity<?>findByEmployeeId(@PathVariable String id){
+        try{
+            return new ResponseEntity<>(employeeService.findByEmployeeId(id), HttpStatus.OK);
+        }catch (EmployeeNotFoundException e){
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/api/v1/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable String id, @RequestBody Employee employeeDetails){
+      try {
+          return new ResponseEntity<>(employeeService.updateEmployee(id,employeeDetails), HttpStatus.OK);
+      }catch (EmployeeNotFoundException e){
+          return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+      }
+    }
+
+
+     @DeleteMapping("/api/v1/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable String id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(()->
+                new EmployeeNotFoundException("employee does not exist"));
+
+        employeeRepository.delete(employee);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
--- src/main/java/africa/semicolon/backendemployeemanagementsystem/data/dtos/request/CreateEmployeeRequest.java
+++ src/main/java/africa/semicolon/backendemployeemanagementsystem/data/dtos/request/CreateEmployeeRequest.java
@@ -12,7 +12,7 @@ public class CreateEmployeeRequest {
 
     private String lastName;
 
-    private String email;
+    private String emailId;
 
     private String userName;
 
