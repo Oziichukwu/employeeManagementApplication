5bb03786fed46d09b45e2a8d74b09857d4e47148
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-09 06:11:00 +0100

    api response added

--- src/main/java/africa/semicolon/backendemployeemanagementsystem/data/dtos/response/ApiResponse.java
+++ src/main/java/africa/semicolon/backendemployeemanagementsystem/data/dtos/response/ApiResponse.java
@@ -1,2 +1,21 @@
-package africa.semicolon.backendemployeemanagementsystem.data.dtos.response;public class ApiResponse {
+package africa.semicolon.backendemployeemanagementsystem.data.dtos.response;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse {
+
+    private boolean isSuccessful;
+
+    private String message;
+
+    private LocalDateTime responseDate;
+
+    public ApiResponse(boolean isSuccessful, String message) {
+        this.isSuccessful = isSuccessful;
+        this.message = message;
+        responseDate = LocalDateTime.now();
+    }
 }
